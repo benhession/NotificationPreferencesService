28c63acf50727af34291734a8a694c87dffaf400
Ben Hession <bshession@gmail.com>
2022-12-27 18:10:41 +0000

    Initial commit

---
+++ src/main/java/com/benhession/notificationpreferencesservice/NotificationPreferencesServiceApplication.java
@@ -0,0 +1,12 @@
+package com.benhession.notificationpreferencesservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotificationPreferencesServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NotificationPreferencesServiceApplication.class, args);
+    }
+}
---
+++ src/test/java/com/benhession/notificationpreferencesservice/NotificationPreferencesServiceApplicationTests.java
@@ -0,0 +1,12 @@
+package com.benhession.notificationpreferencesservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationPreferencesServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
